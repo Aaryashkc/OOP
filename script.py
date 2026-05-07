5ab8545811b834ce95de07ca9b4dee132e4f3a32
Aaryashkc <aaryashkc@outlook.com>
2026-05-07 11:56:46 +0545

    exercise

--- script.py
+++ script.py
@@ -9,4 +9,21 @@ class PlayerCharacter:
 
 player1 = PlayerCharacter("aaryash", 50, 9855067253)
 
-print(player1.name, player1.age, player1.phone)
+# print(player1.name, player1.age, player1.phone)
+
+
+class Cat:
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age=age
+
+
+cat1=Cat('denail', 20)
+cat2=Cat('den', 18)
+cat3=Cat('dena', 90)
+
+def oldest_cat(*args):
+    return max(args)
+
+print(oldest_cat(cat1.age, cat2.age, cat3.age))
