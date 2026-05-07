1db6e99b42c291df2f5e94eb8c30bf1773f3984c
Aaryashkc <aaryashkc@outlook.com>
2026-05-07 15:37:14 +0545

    exercise

--- script.py
+++ script.py
@@ -9,7 +9,7 @@ class PlayerCharacter:
 
 player1 = PlayerCharacter("aaryash", 50, 9855067253)
 
-# print(player1.name, player1.age, player1.phone)
+print(player1.name, player1.age, player1.phone)
 
 
 class Cat:
@@ -17,6 +17,11 @@ class Cat:
     def __init__(self, name, age):
         self.name = name
         self.age=age
+
+    @classmethod
+    def adding_age(cls, age1, age2):
+        return age1+age2
+
 
 
 cat1=Cat('denail', 20)
@@ -26,4 +31,7 @@ cat3=Cat('dena', 90)
 def oldest_cat(*args):
     return max(args)
 
-print(oldest_cat(cat1.age, cat2.age, cat3.age))
+print(f'the oldest cat is {oldest_cat(cat1.age, cat2.age, cat3.age)} years old')
+print(f'The sum of cat1 and cat 2 is {Cat.adding_age(cat1.age, cat2.age)}')
+
+
