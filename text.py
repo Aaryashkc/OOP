6bbdd6e0a3bf59a33f402dc44684e70740794bdb
Aaryashkc <aaryashkc@outlook.com>
2026-05-09 08:36:56 +0545

    push_exercise

--- text.py
+++ text.py
@@ -13,3 +13,19 @@ Person1=Person('Aaryash', 20)
 
 print(Person1.call_name())
 Person1.call_age()
+
+
+class Gun:
+    def __init__(self, damage, ammo):
+        self.damage= damage
+        self.ammo= ammo
+
+    def deal_damage(self):
+        print(f'Player deal {self.damage} damage')
+
+    def deal_ammo(self):
+        print(f'Player has {self.ammo} ammo')
+
+Round1=Gun(1000, 100)
+Round1.deal_damage()
+Round1.deal_ammo()
